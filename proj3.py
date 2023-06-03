769cd567344ae09369189a36f67728ecc0bf6e1b
Henrique Martins <henrique.martins99@hotmail.com>
2023-06-03 21:53:54 +0100

    final version

--- proj3.py
+++ proj3.py
@@ -38,6 +38,10 @@ def train_rbfn(X, y, num_centers, rbf_type, alpha=0.01):
     else:
         raise ValueError("Invalid RBF type")
     
+    # activation layer
+    #X_h = 1 / (1 + np.exp(-X_transformed))
+    #X_h = np.exp(-X_transformed ** 2)
+
     # Minimizes the objective function: ||y - Xw||^2_2 + alpha * ||w||^2_2
     model = Ridge(alpha=alpha)
     model.fit(X_transformed, y)
@@ -56,6 +60,12 @@ def predict_rbfn(X, model, centers, rbf_type):
         X_transformed = thin_plate_spline_rbf(X, centers)
     else:
         raise ValueError("Invalid RBF type")
+    
+    
+    # activation layer
+    #X_h = 1 / (1 + np.exp(-X_transformed))
+    #X_h = np.exp(-X_transformed ** 2)
+
     return model.predict(X_transformed)
 
 # Set random seed for reproducibility
@@ -63,7 +73,7 @@ np.random.seed(42)
 
 # Generate data for two nonlinear functions
 # Sine Function
-x = np.linspace(-5, 5, 400).reshape(-1, 1)
+x = np.linspace(-10, 10, 1000).reshape(-1, 1)
 # Split the data into train and test sets
 x1, X_test = train_test_split(x, test_size=0.3)
 x1, X_test = np.sort(x1, axis=0), np.sort(X_test, axis=0)
@@ -71,7 +81,7 @@ y1 = np.sin(x1) + np.random.normal(0, 0.1, x1.shape)
 y_test = np.sin(X_test) + np.random.normal(0, 0.1, X_test.shape)
 
 # Exponential Decay Function
-x = np.linspace(0, 10, 400).reshape(-1, 1)
+x = np.linspace(0, 400, 1000).reshape(-1, 1)
 x2, X_test2 = train_test_split(x, test_size=0.3)
 x2, X_test2 = np.sort(x2, axis=0), np.sort(X_test2, axis=0)
 y2 = np.exp(-x2 / 2) + np.random.normal(0, 0.05, x2.shape)
@@ -86,6 +96,7 @@ mse_values_train = []
 mse_values_test = []
 mse_values_train2 = []
 mse_values_test2 = []
+i = 1
 for rbf_type in rbf_types:
     for alpha in reg_values:
         # Train RBFN
@@ -111,58 +122,82 @@ for rbf_type in rbf_types:
 
         # Plot the results
         # region RBNF Approximation 
+        plt.figure(figsize=(6.9, 4.8))
         plt.scatter(x1, y1, label='Sine Data (Train)')
         plt.scatter(X_test, y_test, label='Sine Data (Test)')
         plt.plot(X_test, y_pred, color='green', label='RBFN Approximation (Test)')
         plt.plot(x1, y_pred_train, color='red', label='RBFN Approximation (Train)')
         plt.xlabel('X')
         plt.ylabel('y')
-        plt.title(f'Function: Sine, RBF Type: {rbf_type}, alpha: {alpha}')
+        plt.title(f'Sine, RBF: {rbf_type}, alpha: {alpha}')
         plt.legend()
-        plt.show()
-        
-        plt.scatter(x2, y2, label='Exponential Decay Data (Train)')
-        plt.scatter(X_test2, y_test2, label='Exponential Decay Data (Test)')
+        plt.savefig(f'Images/RESULTS/{i} - Sine, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
+
+        plt.figure(figsize=(6.9, 4.8))
+        plt.scatter(x2, y2, label='Exp Decay Data (Train)')
+        plt.scatter(X_test2, y_test2, label='Exp Decay Data (Test)')
         plt.plot(X_test2, y_pred2, color='green', label='RBFN Approximation (Test)')
         plt.plot(x2, y_pred2_train, color='red', label='RBFN Approximation (Train)')
         plt.xlabel('X')
         plt.ylabel('y')
-        plt.title(f'Function: Exponential Decay, RBF Type: {rbf_type}, alpha: {alpha}')
+        plt.title(f'Exp Decay, RBF: {rbf_type}, alpha: {alpha}')
         plt.legend()
-        plt.show()
+        plt.savefig(f'Images/RESULTS/{i} - Exp Decay, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
         # endregion
 
         # region Residuals
         residuals = y_pred - y_test
+        plt.figure(figsize=(6.9, 4.8))
         plt.scatter(X_test, residuals)
         plt.axhline(y=0, color='red', linestyle='--')
         plt.xlabel('X')
         plt.ylabel('Residuals')
-        plt.title(f'Residual Plot - Function: Sine, RBF Type: {rbf_type}, alpha: {alpha}')
-        plt.show()
+        plt.title(f'Residual Plot - Sine, RBF: {rbf_type}, alpha: {alpha}')
+        plt.savefig(f'Images/RESULTS/{i} - Residual Plot - Sine, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
         residuals = y_pred2 - y_test2
+        plt.figure(figsize=(6.9, 4.8))
         plt.scatter(X_test2, residuals)
         plt.axhline(y=0, color='red', linestyle='--')
         plt.xlabel('X')
         plt.ylabel('Residuals')
-        plt.title(f'Residual Plot - Function: Exponential Decay, RBF Type: {rbf_type}, alpha: {alpha}')
-        plt.show()
+        plt.title(f'Residual Plot - Exp Decay, RBF: {rbf_type}, alpha: {alpha}')
+        plt.savefig(f'Images/RESULTS/{i} - Residual Plot - Exp Decay, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
         # endregion Residuals
 
         # region Error Distribution
         errors = y_pred - y_test
+        plt.figure(figsize=(6.9, 4.8))
         plt.hist(errors, bins=20)
         plt.xlabel('Error')
         plt.ylabel('Frequency')
-        plt.title(f'Error Distribution (Test) - Function: Exponential Decay, RBF Type: {rbf_type}, alpha: {alpha}')
-        plt.show()
+        plt.title(f'Error Distribution (Test) - Sine, RBF: {rbf_type}, alpha: {alpha}')
+        plt.savefig(f'Images/RESULTS/{i} - Error Distribution (Test) - Sine, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
         errors = y_pred2 - y_test2
+        plt.figure(figsize=(6.9, 4.8))
         plt.hist(errors, bins=20)
         plt.xlabel('Error')
         plt.ylabel('Frequency')
-        plt.title(f'Error Distribution (Test) - Function: Exponential Decay, RBF Type: {rbf_type}, alpha: {alpha}')
-        plt.show()
-        # endregion Residuals
+        plt.title(f'Error Distribution (Test) - Exp Decay, RBF: {rbf_type}, alpha: {alpha}')
+        plt.savefig(f'Images/RESULTS/{i} - Error Distribution (Test) - Exp Decay, {rbf_type}, {alpha}.png')
+        #plt.show()
+        plt.close()
+        i += 1
+        # endregion Residuals"""
 
 # Sort MSE values in ascending order
 mse_values_train.sort(key=lambda x: x[2])
@@ -173,36 +208,51 @@ mse_values_test2.sort(key=lambda x: x[2])
 # Plot the MSE values
 configurations = [f'{rbf}_{alpha}' for (rbf, alpha, _) in mse_values_train]
 mse_scores = [mse for (_, _, mse) in mse_values_train]
+ac_function = "NO ACT"
+#ac_function = "SIGMOID"
+#ac_function = "GAUSSIAN"
 
+plt.figure(figsize=(17,6))
 plt.barh(configurations, mse_scores)
 plt.xlabel('Mean Squared Error (MSE)')
 plt.ylabel('Configuration')
 plt.title('Mean Squared Error - Sine (Train)')
+plt.savefig(f'Images/1 - NO ACT vs SIGMOID vs GAUSSIAN/Mean Squared Error - Sine (Train) - {ac_function}.png')
 plt.show()
+#plt.clf()
 
 configurations = [f'{rbf}_{alpha}' for (rbf, alpha, _) in mse_values_test]
 mse_scores = [mse for (_, _, mse) in mse_values_test]
 
+plt.figure(figsize=(17,6))
 plt.barh(configurations, mse_scores)
 plt.xlabel('Mean Squared Error (MSE)')
 plt.ylabel('Configuration')
 plt.title('Mean Squared Error - Sine (Test)')
+plt.savefig(f'Images/2 - NO ACT vs SIGMOID vs GAUSSIAN/Mean Squared Error - Sine (Test) - {ac_function}.png')
 plt.show()
+#plt.clf()
 
 configurations = [f'{rbf}_{alpha}' for (rbf, alpha, _) in mse_values_train2]
 mse_scores = [mse for (_, _, mse) in mse_values_train2]
 
+plt.figure(figsize=(17,6))
 plt.barh(configurations, mse_scores)
 plt.xlabel('Mean Squared Error (MSE)')
 plt.ylabel('Configuration')
-plt.title('Mean Squared Error - Exponential Decay (Train)')
+plt.title('Mean Squared Error - Exp Decay (Train)')
+plt.savefig(f'Images/3 - NO ACT vs SIGMOID vs GAUSSIAN/Mean Squared Error - Exp Decay (Train) - {ac_function}.png')
 plt.show()
+#plt.clf()
 
 configurations = [f'{rbf}_{alpha}' for (rbf, alpha, _) in mse_values_test2]
 mse_scores = [mse for (_, _, mse) in mse_values_test2]
 
+plt.figure(figsize=(17,6))
 plt.barh(configurations, mse_scores)
 plt.xlabel('Mean Squared Error (MSE)')
 plt.ylabel('Configuration')
-plt.title('Mean Squared Error - Exponential Decay (Test)')
+plt.title('Mean Squared Error - Exp Decay (Test)')
+plt.savefig(f'Images/4 - NO ACT vs SIGMOID vs GAUSSIAN/Mean Squared Error - Exp Decay (Test) - {ac_function}.png')
 plt.show()
+#plt.clf()
